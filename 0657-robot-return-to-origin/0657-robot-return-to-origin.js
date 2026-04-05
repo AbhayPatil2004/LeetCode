7f502d55a9a17dfcc9663d97e1b1216b928fcfb8
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2026-04-05 15:31:57 +0530

    Time: 7 ms (47.83%), Space: 55.6 MB (68.23%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    
+        let x = 0 , y = 0 ;
+
+        for( let i = 0 ; i < moves.length ; i++ ){
+            if( moves[i] == 'U' ) x ++ ;
+            else if( moves[i] == 'D' ) x -- ;
+            else if( moves[i] == 'L' ) y ++ ;
+            else{
+                y -- ;
+            }
+        }
+
+        if( x != 0 || y != 0 ) return false ;
+        return true ;
+};
